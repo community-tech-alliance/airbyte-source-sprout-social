8f32608ffbc14aace11b931a7c93947ea391b633
kane <kanelouise@users.noreply.github.com>
2023-12-15 20:29:28 -0800

    feat: add all streams (commented out)

--- source_sprout_social/source.py
+++ source_sprout_social/source.py
@@ -34,7 +34,7 @@ class SproutSocialStream(HttpStream, ABC):
         stream_slice: Mapping[str, Any] = None,
         next_page_token: Mapping[str, Any] = None,
     ) -> Mapping[str, Any]:
-        return {"Authorization": f"Bearer {self.config[{api_key}]}" }
+        return {"Authorization": f"Bearer {self.config['api_key']}" }
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """Pagination for most endpoints (except messages) is acheived by incrementing `/page/#` in the request URL.
@@ -55,11 +55,11 @@ class SproutSocialStream(HttpStream, ABC):
         next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         """can probably comment out for prelim testing"""
-        params = super().request_params(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
-        params["PageSize"] = self.page_size
-        if next_page_token:
-            params.update(**next_page_token)
-        return params
+        # params = super().request_params(stream_state=stream_state, stream_slice=stream_slice, next_page_token=next_page_token)
+        # params["PageSize"] = self.page_size
+        # if next_page_token:
+        #     params.update(**next_page_token)
+        # return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
@@ -81,90 +81,146 @@ class ClientMetadata(SproutSocialStream):
     ) -> str:
         endpoint = "metadata/client"
         return endpoint
+    
+class ClientMetadata(SproutSocialStream):
+    primary_key = "customer_id"
 
+    def path(
+        self, stream_state: Mapping[str, Any] = None, 
+        stream_slice: Mapping[str, Any] = None, 
+        next_page_token: Mapping[str, Any] = None,
+        **kwargs,
+    ) -> str:
+        endpoint = "metadata/client"
+        return endpoint
+    
+# class CustomerProfiles(SproutSocialStream):
+#     primary_key = "customer_profile_id"
+#     """This request needs to grab customer_id from ClientMetadata returned
+#     will be received from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`"""
 
-# # Basic incremental stream
-# class IncrementalSproutSocialStream(SproutSocialStream, ABC):
-#     """
-#     TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-#          if you do not need to implement incremental sync for any streams, remove this class.
-#     """
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/metadata/client"
+#         return endpoint
+    
+# class CustomerTags(SproutSocialStream):
+#     primary_key = "tag_id"
+#     """This request needs to grab customer_id from ClientMetadata returned
+#     will be received from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`"""
 
-#     # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-#     state_checkpoint_interval = None
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/metadata/customer/tags"
+#         return endpoint
+    
+# class CustomerGroups(SproutSocialStream):
+#     primary_key = "group_id"
+#     """This request needs to grab customer_id from ClientMetadata returned
+#     will be received from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`"""
 
-#     @property
-#     def cursor_field(self) -> str:
-#         """
-#         TODO
-#         Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-#         usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/metadata/customer/groups"
+#         return endpoint
+    
+# class CustomerUsers(SproutSocialStream):
+#     primary_key = "group_id"
+#     """This request needs to grab customer_id from ClientMetadata returned
+#     will be received from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`"""
 
-#         :return str: The name of the cursor field.
-#         """
-#         return []
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/metadata/customer/users"
+#         return endpoint
+    
+# class CustomerProfileAnalytics(SproutSocialStream):
+#     primary_key = "customer_profile_id"
+#     """This endpoint retrieves data from the `analytics/profiles` endpoint as a post request.   
+#     The request needs: 
+#       - a customer_id from ClientMetadata returned from from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`,
+#       - a list (but saved as a string) from the `metadata/customer` endpoint based on `network_type` (aka social media site)
+#       - a json specifically filtered for each `network_type` (aka social media site) 
+#         (in colab notebook, uses `post_api` function and `facebook_analytics_profiles`, `instagram_analytics_profiles`, and `tiktok_analytics_profiles` as json data)       
+#      """
 
-#     def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-#         """
-#         Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-#         the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-#         """
-#         return {}
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/analytics/profiles"
+#         return endpoint
+    
+# class ProfileAnalytics(SproutSocialStream):
+#     primary_key = "customer_profile_id"
+#     """This endpoint retrieves data from the `analytics/profiles` endpoint as a post request.   
+#     The request needs: 
+#       - a customer_id from ClientMetadata returned from from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`,
+#       - a list (but saved as a string) from the `metadata/customer` endpoint based on `network_type` (aka social media site)
+#         (in colab notebook these string lists are saved as: `facebook`,`instagram`,`tiktok` and were not created programmatically)       
+#       - a json specifically filtered for each `network_type` (aka social media site) 
+#         (in colab notebook, uses `post_api` function and `facebook_analytics_profiles`, `instagram_analytics_profiles`, and `tiktok_analytics_profiles` as json data)       
+#      """
 
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/analytics/profiles"
+#         return endpoint
 
-# class Employees(IncrementalSproutSocialStream):
-#     """
-#     TODO: Change class name to match the table/data source this stream corresponds to.
-#     """
+# class PostAnalytics(SproutSocialStream):
+#     primary_key = "customer_profile_id"
+#     """This endpoint retrieves data from the `analytics/profiles` endpoint as a post request.   
+#     The request needs: 
+#       - a customer_id from ClientMetadata returned from from `{json_returned_by_ClientMetadata}['data'][0]['customer_id']`,
+#       - a list (but saved as a string) from the `metadata/customer` endpoint based on `network_type` (aka social media site)
+#         (in colab notebook these string lists are saved as: `facebook`,`instagram`,`tiktok` and were not created programmatically)       
+#       - a json specifically filtered for each `network_type` (aka social media site) 
+#         (in colab notebook, uses `post_api` function and `facebook_analytics_posts`, `instagram_analytics_posts`, and `tiktok_analytics_posts` as json data)       
+#      """
 
-#     # TODO: Fill in the cursor_field. Required.
-#     cursor_field = "start_date"
-
-#     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-#     primary_key = "employee_id"
-
-#     def path(self, **kwargs) -> str:
-#         """
-#         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-#         return "single". Required.
-#         """
-#         return "employees"
-
-#     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-#         """
-#         TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-#         Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-#         This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-#         section of the docs for more information.
-
-#         The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-#         necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-#         This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-#         An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-#         craft that specific request.
-
-#         For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-#         this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-#         till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-#         the date query param.
-#         """
-#         raise NotImplementedError("Implement stream slices or delete this method!")
-
+#     def path(
+#         self, stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None,
+#         **kwargs,
+#     ) -> str:
+#         endpoint = f"{customer_id}/analytics/posts"
+#         return endpoint
 
 # # Source
 class SourceSproutSocial(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
-        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
         :param config:  the user-input config object conforming to the connector's spec.yaml
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
         api_key = config['api_key']
         connection_url = f"https://api.sproutsocial.com/v1/metadata"
-        headers = {'Authorization': f"Bearer {api_key}"}
+        api_key = self.config['api_key']
+        headers = {'Authorization': f"Bearer {self.config['api_key']}"}
         try:
             response = requests.get(url=connection_url, headers=headers)
             response.raise_for_status()
